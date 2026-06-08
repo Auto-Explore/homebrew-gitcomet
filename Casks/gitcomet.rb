47a44fe2798075ceae5b054d93c278d0deb2f600
Francesco Lucarelli <67560467+fraluc06@users.noreply.github.com>
2026-06-08 14:26:58 +0200

    gitcomet: replace deprecated depends_on
    Replace deprecated depends_on macos: ">= :ventura"
    with new depends_on macos: :ventura
    (This will have the same behavior as before.)

--- Casks/gitcomet.rb
+++ Casks/gitcomet.rb
@@ -1,32 +1,23 @@
 cask "gitcomet" do
-  version "0.1.15"
   arch arm: "arm64", intel: "x86_64"
   os macos: "macos", linux: "linux"
 
-  on_macos do
-    on_arm do
-      sha256 "c4c1b1fcb316194fd19288f7c41a9426c599d8b2c3b4b4be8491dc75df372e01"
-    end
+  version "0.1.15"
 
-    on_intel do
-      sha256 "9a649d0ae089db8eab283c5607b486509472d2324545005d827a9365dcf60c50"
-    end
+  on_macos do
+    sha256 arm:   "c4c1b1fcb316194fd19288f7c41a9426c599d8b2c3b4b4be8491dc75df372e01",
+           intel: "9a649d0ae089db8eab283c5607b486509472d2324545005d827a9365dcf60c50"
 
     url "https://github.com/Auto-Explore/GitComet/releases/download/v#{version}/gitcomet-v#{version}-macos-#{arch}.dmg"
-    depends_on macos: ">= :ventura"
+    depends_on macos: :ventura
 
     app "GitComet.app"
     binary "#{appdir}/GitComet.app/Contents/MacOS/gitcomet", target: "gitcomet"
   end
 
   on_linux do
-    on_arm do
-      sha256 "acc9d5549ea6056a3cd6f2c127441eb139e88531f665f8c85d6fbfcbaecac7e0"
-    end
-
-    on_intel do
-      sha256 "ca35009d44fb92907ee57dda92085d143ba532744a5685366b1abab8a4440f14"
-    end
+    sha256 arm:   "acc9d5549ea6056a3cd6f2c127441eb139e88531f665f8c85d6fbfcbaecac7e0",
+           intel: "ca35009d44fb92907ee57dda92085d143ba532744a5685366b1abab8a4440f14"
 
     url "https://github.com/Auto-Explore/GitComet/releases/download/v#{version}/gitcomet-v#{version}-linux-#{arch}.AppImage"
     container type: :naked
